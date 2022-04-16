9ba597ad8aa217df07a5e39f48834a2da464f4d1
champkeh <champion.ge@gmail.com>
2022-04-16 12:46:38 +0800

    添加epidemic页面

---
+++ epidemic/mod.ts
@@ -0,0 +1,15 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
+
+async function handleRequest(request: Request): Promise<Response> {
+    const { pathname } = new URL(request.url)
+
+    const file = await Deno.readFile("./index.html")
+    return new Response(file, {
+        headers: {
+            "Content-Type": "text/html; charset=utf-8",
+        }
+    })
+}
+
+console.log("Listening on http://localhost:8000")
+serve(handleRequest)
